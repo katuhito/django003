2729713b1b989df4669f73a9278ed591195102c6
Katuhito Hara <rahakatuhito@gmail.com>
2022-03-25 16:25:16 +0900

    Comments実装

--- first_app/urls.py
+++ first_app/urls.py
@@ -5,6 +5,5 @@ app = 'first_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('page/<int:page_id>/', views.page, name='page'),
-    path('article/', views.article, name='article'),
+    path('article/<int:article_id>/', views.article, name='article'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,19 +1,21 @@
-from email import message
 from django.http import HttpResponse
 from django.template import loader
+from .models import Article
 
-def article(request):
-    template = loader.get_template('article.html')
+def index(request):
+    template = loader.get_template('index.html')
+    articles = Article.objects.order_by('-created_at')[:5]
     context = {
-        'title': '記事のタイトル',
-        'content': '記事の本文',
+        'articles': articles
     }
     return HttpResponse(template.render(context, request))
 
-def index(request):
-    message = 'Hello, world!',
-    return HttpResponse(message)
+def article(request, article_id):
+    article = Article.objects.get(pk=article_id)
+    template = loader.get_template('article.html')
+    context = {
+        'article': article
+    }
+    return HttpResponse(template.render(context, request))
 
-def page(request, page_id):
-    message = 'ページ' + str(page_id)
-    return HttpResponse(message)
+
